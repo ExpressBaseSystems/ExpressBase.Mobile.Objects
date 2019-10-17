37a01bc4ab6a84ac714021aad69031894cd8ad4f
Amalkdev <amaldev@expressbase.com>
2019-10-17 23:45:25 +0530

    eb.mob.obj

---
+++ ObjectContainers/EbWebForm.cs
@@ -0,0 +1,30 @@
+﻿using ExpressBase.Mobile.Common;
+using ExpressBase.Mobile.Common.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Mobile.Objects
+{
+    public class EbWebForm : EbForm
+    { 
+
+    }
+
+    public class EbControlWrapper
+    {
+        public string TableName { get; set; }
+
+        public string Path { get; set; }
+
+        public string Root { get; set; }
+
+        public EbControl Control { get; set; }
+    }
+}
