1ac2c873de172bd19696e5453feafb0202ab25ad
Amalkdev <amaldev@expressbase.com>
2019-10-20 21:46:31 +0530

    mobile.object

---
+++ Objects/EbDate.cs
@@ -0,0 +1,24 @@
+﻿using ExpressBase.Mobile.Common.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Mobile.Objects
+{
+    public class EbDate : EbControlUI
+    {
+        public DateTime Min { get; set; }
+
+        public DateTime Max { get; set; }
+
+        public DateTime Value { get; set; }
+
+        public string PlaceHolder { get; set; }
+
+        public bool AutoCompleteOff { get; set; }
+
+        public override bool DoNotPersist { get; set; }
+
+        public bool IsNullable { get; set; }
+    }
+}
