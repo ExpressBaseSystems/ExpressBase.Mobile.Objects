0f4aa5fb99299e28e876fbd3da27d34e7500d16d
Amalkdev <amaldev@expressbase.com>
2019-10-24 20:56:19 +0530

    mobile.obj

---
+++ ObjectContainers/EbGroupBox.cs
@@ -0,0 +1,14 @@
+﻿using ExpressBase.Mobile.Common.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressBase.Mobile.Objects
+{
+    public class EbGroupBox : EbControlContainer
+    {
+        public override string Label { get; set; }
+
+        public bool HideBorder { get; set; }
+    }
+}
